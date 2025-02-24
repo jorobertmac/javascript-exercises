6b6f8c58c5faad0c75b909b2835b173b378549ca
jorobertmac <160981126+jorobertmac@users.noreply.github.com>
2025-02-23 18:14:03 -0700

    Allow tests pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let reversed = ""
+  for (let i = string.length; i > 0; i--) {
+    reversed += string[i-1]
+  }
+  return reversed
 };
 
 // Do not edit below this line
