ea17177c3b623fefb6faa6f3a4ccdd3e38c17875
jorobertmac <160981126+jorobertmac@users.noreply.github.com>
2025-02-23 18:14:16 -0700

    Allow tests pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+  for (let arg of remove){
+    while (array.includes(arg)) {
+      let index = array.indexOf(arg)
+      array.splice(index, 1)
+    }
+  }
+  return array
 };
 
 // Do not edit below this line
