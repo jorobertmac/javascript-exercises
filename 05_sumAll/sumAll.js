d35ddc2da15342efc11502b374e8f41744e94980
jorobertmac <160981126+jorobertmac@users.noreply.github.com>
2025-02-23 18:31:32 -0700

    Allow tests pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(start, stop) {
+  if (
+    typeof start !== 'number' ||
+    typeof stop !== 'number' ||
+    start < 0 ||
+    stop < 0 ||
+    start % 1 !== 0 ||
+    stop % 1 !== 0
+    ) {
+    return 'ERROR'
+  }
+  let sum = 0
+  if (start > stop){
+    newStart = stop
+    stop = start
+    start = newStart
+  }
+  for (let i = start; i <= stop; i++){
+    sum += i
+  }
+  return sum
 };
 
 // Do not edit below this line
