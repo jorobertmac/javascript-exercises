c16bf0429936ebcf5e6583bd6cfb78d919126cce
jorobertmac <160981126+jorobertmac@users.noreply.github.com>
2025-04-08 20:08:46 -0700

    Allow tests pass

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const validChar = "abcdefghijklmnopqrstuvwxyz0123456789"
 
+  const forward = string
+  .toLowerCase()
+  .split("")
+  .filter((character) => validChar.includes(character))
+  .join("")
+
+  const reversed = forward.split("").reverse().join("")
+
+  return forward === reversed
 };
 
 // Do not edit below this line
