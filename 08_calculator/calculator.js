68be0f23bc3be8c3f567e3aded7a9c9fb9cd0218
jorobertmac <160981126+jorobertmac@users.noreply.github.com>
2025-04-08 19:32:23 -0700

    Allow tests pass

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(numsArray) {
+	return numsArray.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numsArray) {
+  return numsArray.reduce((total, current) => total * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(factor) {
+  let total = 1
+	for (let i = 1; i <= factor; i++) {
+    total *= i
+  }
+  return total
 };
 
 // Do not edit below this line
