309ca8d298935a950e6ccd2af30954f872d9b507
jorobertmac <160981126+jorobertmac@users.noreply.github.com>
2025-04-09 18:59:52 -0700

    Allow tests pass

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let oldestAge = 0
+  let oldestPerson
+  people.forEach((person) => {
+    let birth = person.yearOfBirth
+    let death
+    if (typeof person.yearOfDeath === "undefined") {
+      death = 2025
+    } else {
+      death = person.yearOfDeath
+    }
+    
+    const age = death - birth
+    if (age > oldestAge) {
+      oldestAge = age
+      oldestPerson = person
+    }
+  })
 
+  return oldestPerson
 };
 
 // Do not edit below this line
