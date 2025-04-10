c2dfe30633deaf92d074bfed7e966bc71144c545
jorobertmac <160981126+jorobertmac@users.noreply.github.com>
2025-04-09 17:28:58 -0700

    Allow tests pass

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  number = parseInt(number)
+  if (isNaN(number) || number < 0) return "OOPS"
+  if (number === 0) return 0
+  if (number === 1) return 1
 
+  let result = 0
+  let previous = 0
+  let current = 1
+  for (let i = 1; i < number; i++) {
+    result = previous + current
+    previous = current
+    current = result
+  }
+  return result
 };
 
 // Do not edit below this line
